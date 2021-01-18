fcfa5a3383126de1f1c967c5a65bc51bd4afd26d
Hexiro <realhexiro@gmail.com>
2021-01-18 15:32:21 -0500

    fixed logging and updated db to json format

--- herokron/herokron.py
+++ herokron/herokron.py
@@ -1,4 +1,5 @@
-from sys import exit
+from json import load, dump
+from sys import exit, argv
 from sys import platform
 from os import environ
 from inspect import isfunction
@@ -7,7 +8,6 @@ from datetime import datetime
 from argparse import ArgumentParser
 from pathlib import Path
 
-from dotenv import load_dotenv
 from heroku3 import from_key
 from dhooks import Embed
 from dhooks import Webhook
@@ -35,7 +35,7 @@ def get_datadir() -> Path:
 
 
 def get_datafile() -> Path:
-    return get_datadir() / ".env"
+    return get_datadir() / "db.json"
 
 
 try:
@@ -43,13 +43,12 @@ try:
 except FileExistsError:
     pass
 try:
-    open(get_datafile(), "x").close()
+    dump({"keys": [], "color": 0x4169e1, "webhook": ""}, open(get_datafile(), "x"))
 except FileExistsError:
     pass
 
 
-
-load_dotenv(get_datafile())
+database = load(open(get_datafile()))
 calls = []
 returns = []
 
@@ -63,19 +62,25 @@ def log_message(func, title):
     hook.send(embed=log_embed)
 
 
+class AppWithoutProcfile(Exception):
+    pass
+
+
 class Herokron:
 
-    def __init__(self, name=None):
-        self.keys = [environ[key] for key in [key for key in environ.keys() if key.startswith("HEROKU_KEY")]]
-        if name is not None:
-            for key in self.keys:
-                if name in [app.name for app in from_key(key).apps()]:
-                    self.heroku = from_key(key)
-                    self.app = self.heroku.app(name)
-                    break
+    def __init__(self, app=None):
+        self.keys = database["keys"]
+        if app is not None:
+            if app not in all_apps():
+                refresh_all_apps()
+            if app in all_apps():
+                self.heroku = from_key(key_from_app(app))
+                self.app = self.heroku.app(app)
         if not hasattr(self, "heroku"):
             self.heroku = from_key(self.keys[0])
             self.app = self.heroku.app(self.heroku.apps()[0].name)
+        if not self.app.process_formation():
+            raise AppWithoutProcfile("App hasn't explicitly stated weather it's a worker or a web app.")
         self.proc_type = "worker" if "worker" in self.app.process_formation() else "web"
 
     def __getattribute__(self, name):
@@ -167,11 +172,44 @@ def is_on(name):
     return Herokron(name).is_on()
 
 
+def refresh_all_apps(write=True):
+    for key in database["keys"]:
+        key = key["key"]
+        refresh_apps(key, write)
+    return all_apps()
+
+
+def refresh_apps(key, write=True):
+    search = list(filter(lambda keys: keys["key"] == key, database["keys"]))
+    index = database["keys"].index(search[0])
+    apps = [app.name for app in from_key(key).apps()]
+    database["keys"][index]["apps"] = apps
+    if write:
+        dump_database()
+    return apps
+
+
+def all_apps():
+    return [item for sublist in [key["apps"] for key in database["keys"]] for item in sublist]
+
+
+def key_from_app(app):
+    for num in range(len(database["keys"])):
+        if app in database["keys"][num]["apps"]:
+            return database["keys"][num]["key"]
+
+
+def dump_database():
+    dump(database, open(get_datafile(), "w"))
+
+
 def main():
     parser = ArgumentParser()
-    parser.add_argument("func",
-                        help="The name of the function to call.")
-    parser.add_argument("app",
+    parser.add_argument("-func",
+                        help="The name of the function to call.",
+                        nargs="?",
+                        default=None)
+    parser.add_argument("-app",
                         help="The name of the Heroku app.",
                         nargs="?",
                         default=None)
@@ -180,21 +218,69 @@ def main():
                         nargs="?",
                         help="Stops this iteration from logging.",
                         default=False)
+    parser.add_argument("--add-key",
+                        "-a",
+                        help="Adds the Heroku API key specified.",
+                        default=False)
+    parser.add_argument("--remove-key",
+                        "-r",
+                        help="Removes the Heroku API key specified.",
+                        default=False)
+    parser.add_argument("--set-webhook",
+                        "-w",
+                        help="Sets the Discord Webhook URL for logging.",
+                        default=False)
+    parser.add_argument("--set-color",
+                        "-c",
+                        help="Sets the Discord Embed Color.",
+                        default=False)
+    parser.add_argument("--no-print",
+                        "-p",
+                        help="Doesn't print stored values.",
+                        nargs="?",
+                        default=False)
+    if len(argv) == 1:
+        parser.print_help()
+        return
+
     options = parser.parse_args()
+
+    _add_key = options.add_key
+    _remove_key = options.remove_key
+    _webhook = options.set_webhook
+    _color = options.set_color
+    _no_print = options.no_print
+
+    if _add_key:
+        if not list(filter(lambda keys: keys["key"] == _add_key, database["keys"])):
+            database["keys"].append({"key": _add_key, "apps": []})
+            refresh_apps(_add_key, write=False)
+    if _remove_key:
+        search = list(filter(lambda keys: keys["key"] == _remove_key, database["keys"]))
+        if search:
+            del database["keys"][database["keys"].index(search[0])]
+    if _webhook:
+        database["webhook"] = _webhook
+    if _color:
+        database["color"] = _color
+    if any([_add_key, _remove_key, _webhook, _color]):
+        dump_database()
+        if _no_print is False:
+            print(database)
+
     _func = options.func
     _app = options.app
     _no_log = options.no_log
-    if _func not in globals():
-        parser.print_help()
-        exit(1)
-    if bool(_app) and _func in ["on", "off"]:
-        print(globals()[_func](_app))
-        if environ.get("WEBHOOK", None) is not None:
-            if _no_log is False:
+
+    if _app and _func in ["on", "off"]:
+        if _no_print is False:
+            print(globals()[_func](_app))
+        if database["webhook"]:
+            if _no_log is not False:
                 log_message(_func, _app)
-    elif bool(_app):
+    elif _app:
         print(globals()[_func](_app))
-    else:
+    elif _func:
         print(globals()[_func]())
 
 
