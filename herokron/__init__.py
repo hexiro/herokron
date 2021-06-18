d05e885ebd0dc19af03441241c6963fcdc5d401a
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-06-18 15:37:13 -0400

    remove herokron module from `__init__.py`

--- herokron/__init__.py
+++ herokron/__init__.py
@@ -1,6 +1,9 @@
-__author__ =  "Hexiro"
+__author__ = "Hexiro"
 __version__ = "4.0.0"
 __contact__ = "https://github.com/Hexiro"
 __license__ = "CC0"
+
 from .herokron import Herokron
 from .herokron import on, off, status
+
+del herokron
